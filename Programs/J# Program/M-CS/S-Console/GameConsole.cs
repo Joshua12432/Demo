bf262dbdb252804cf346f4170275e1e136d7639e
Joshua12432 <troublemakerjosh21@gmail.com>
2023-01-14 19:33:55 -0800

    Update 1

---
+++ Programs/J# Program/M-CS/S-Console/GameConsole.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace JSharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Title = "Game Console";
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.WindowHeight = 40;
+            Console.WindowWidth = 40;
+            
+
+            Console.WriteLine("Program:Welcome To the Game C# Launcher");
+            Console.WriteLine("Console:But Wait Before All That Whats Ya Name?");
+            Console.ReadLine();
+            Console.WriteLine("Console:Cool Name! My Name Is OPERATING SYSTEM.. uh Anyway");
+            Console.WriteLine("Console:What do you like to do? Personally i like being a game.");
+            Console.ReadLine();
+            Console.WriteLine("Console:Nice what were we doing again?");
+            Console.ReadLine();
+            Console.WriteLine("Console:Right, ANYWAY byE-Ye-yaye.");
+            Console.WriteLine("Program:\nPress any key to continue....NO THIS CHAT WILL NOT STOP");
+            Console.WriteLine("Console: Ugh hes so annoying. ogga daboogada ");
+            Console.ReadLine();
+            Console.WriteLine("Console:OGGA BOOGA");
+            Console.ReadLine();
+            Console.WriteLine("Console:OGGA BOOGAOGGA BOOGA");
+            Console.ReadLine();
+            Console.WriteLine("Console:The Electric charge of a up quarks is +0.325 and down quarks is -0.125");
+            Console.ReadLine();
+            Console.WriteLine("Console: THERE IS THAT GOOD ProGrAm IS THAT EVEN YOUR REAL NA-");
+            Console.WriteLine("Program:Yes. But also maybe its good enough... NOPE");
+            Console.WriteLine("Console:WHAT THE HE-");
+            Console.ReadLine();
+            Console.WriteLine("Console:JEZE IS THAT GOOD PROGRAM");
+            Console.WriteLine("Program:Yes");
+            Console.WriteLine("Console:Finnaly");
+            Console.WriteLine("User(Probably):hahahahahahahahahahahahadf");
+            Console.WriteLine("User(likely):NOW I CAN ACTUALLY QUIT NOT WITHOUT USING ALT F4");
+            WaitForAnyKeyPressed();
+            
+
+
+            static void WaitForAnyKeyPressed()
+            {
+            Console.WriteLine("\nPress any key to continue....");
+            Console.ReadKey();
+            }                     
+        }
+    }
+}
